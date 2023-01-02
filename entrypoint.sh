8ec6b173bc2253ca97eead29315b5419890adc59
Matthias Preu <5973515+mpreu@users.noreply.github.com>
2023-01-02 10:52:10 +0100

    Update to Alpine 3.17 and squid 5.7
    Also refactor entrypoint script to accomodate for shell script linter
    warnings.
    
    Update workflow.

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,22 +14,22 @@ case $1 in
 esac
 
 echo "Create log directory"
-mkdir -p $SQUID_LOG_DIR
-chmod -R 755 $SQUID_LOG_DIR
-chown -R $SQUID_USER:$SQUID_USER $SQUID_LOG_DIR
+mkdir -p "$SQUID_LOG_DIR"
+chmod -R 755 "$SQUID_LOG_DIR"
+chown -R "$SQUID_USER":"$SQUID_USER" "$SQUID_LOG_DIR"
 
 echo "Create cache directory"  
-mkdir -p $SQUID_CACHE_DIR
-chown -R $SQUID_USER:$SQUID_USER $SQUID_CACHE_DIR
+mkdir -p "$SQUID_CACHE_DIR"
+chown -R "$SQUID_USER":"$SQUID_USER" "$SQUID_CACHE_DIR"
 
 # Run squid
-if [[ -z $1 ]]; then
-  if [[ ! -d $SQUID_CACHE_DIR/00 ]]; then
+if [ -z "$1" ]; then
+  if [ ! -d "$SQUID_CACHE_DIR"/00 ]; then
     echo "Initializing cache directories..."
     $squid_executable -N -f $squid_config -z
   fi
   echo "Starting squid..."
-  exec $squid_executable -f $squid_config -NYCd 1 $EXTRA_ARGS
+  exec $squid_executable -f $squid_config -NYCd 1 "$EXTRA_ARGS"
 else
   exec "$@"
 fi
